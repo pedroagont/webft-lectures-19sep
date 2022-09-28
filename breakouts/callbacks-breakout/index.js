16f792c2a92b88cfde05dab5a5851303ec42e260
Pedro A. González <pgonzalez@post.com>
2022-09-27 22:02:24 -0500

    Add extra example, update readme and include video link

--- breakouts/callbacks-breakout/index.js
+++ breakouts/callbacks-breakout/index.js
@@ -14,5 +14,22 @@ const missingDataMessage = username => {
 
 // CALLING OUR HIGHER ORDER FUNCTION WITH A CALLBACK AS A PARAMETER
 welcomeUser('Nicholas', completeProfileMessage);
-
 welcomeUser('Jeremy', missingDataMessage);
+
+// MATH RESULT CALLBACK EXAMPLE
+// Higher order function
+const result = (val1, val2, callback) => {
+  callback(val1, val2);
+};
+
+// Callbacks declaration
+const sum = (num1, num2) => console.log(num1 + num2);
+const minus = (num1, num2) => console.log(num1 - num2);
+const multiply = (num1, num2) => console.log(num1 * num2);
+const divide = (num1, num2) => console.log(num1 / num2);
+
+// Invoking
+result(2, 2, sum);
+result(2, 2, minus);
+result(2, 2, multiply);
+result(2, 2, divide);
